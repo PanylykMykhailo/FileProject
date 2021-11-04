e78149aadf8f4e20e4696cbd5fcfdfd60b044703
Panylyk Mykhailo <mykhailo.panylyk@gmail.com>
2021-11-04 09:58:08 +0200

    Add model for Deserealize body Add independent method for FE

---
+++ FileService/Model/InfoAboutFile.cs
@@ -0,0 +1,10 @@
+namespace FileService.Model
+{
+    public class InfoAboutFile
+    {
+        public string nameFile{get;set;}
+        public string typeFile{get;set;}
+        public string sizeFile{get;set;}
+        public string dateCreatedFile{get;set;} 
+    }
+}
