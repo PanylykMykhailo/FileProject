f441b96f647ea83fc7bf899fb9a2a87a60207a0d
Panylyk Mykhailo <mykhailo.panylyk@gmail.com>
2021-11-12 00:42:13 +0200

    Open Test folder

--- FileSortService/Program.cs
+++ FileSortService/Program.cs
@@ -21,6 +21,7 @@ namespace FileSortService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseWebRoot("Test");
                 });
     }
 }
